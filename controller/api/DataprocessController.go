cbfb982441989687c708041e7bde1262bba01db4
张丙辰 <10982346+zhang_bingchen@user.noreply.gitee.com>
2024-04-10 16:31:42 +0800

    卡方检验三种情况

--- controller/api/DataprocessController.go
+++ controller/api/DataprocessController.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"gonum.org/v1/gonum/stat"
+	"math"
 	"net/http"
 )
 
@@ -17,8 +18,8 @@ type item struct {
 }
 
 type ChiSquareTest struct {
-	varAValue []int `json:"varAValue"`
-	varBValue []int `json:"varBValue"`
+	VarAValue []int `json:"varAValue"`
+	VarBValue []int `json:"varBValue"`
 }
 
 type AnalysisResult struct {
@@ -38,20 +39,44 @@ func (u DataprocessController) MedicalDataAnalysisHandler(c *gin.Context) {
 	}
 	var table [2][2]float64
 	sum := 0
-	for i := 0; i < len(chisquaredata.varAValue); i++ {
-		sum += chisquaredata.varAValue[i]
+	for i := 0; i < len(chisquaredata.VarAValue); i++ {
+		sum += chisquaredata.VarAValue[i]
 	}
 	table[0][1] = float64(sum)
-	table[0][0] = float64(len(chisquaredata.varAValue) - sum)
+	table[0][0] = float64(len(chisquaredata.VarAValue) - sum)
 
 	sum = 0
-	for i := 0; i < len(chisquaredata.varBValue); i++ {
-		sum += chisquaredata.varBValue[i]
+	for i := 0; i < len(chisquaredata.VarBValue); i++ {
+		sum += chisquaredata.VarBValue[i]
 	}
 	table[1][1] = float64(sum)
-	table[1][0] = float64(len(chisquaredata.varBValue) - sum)
+	table[1][0] = float64(len(chisquaredata.VarBValue) - sum)
 
-	ChiSquare := stat.ChiSquare(table[0][:], table[1][:])
+	var ChiSquare float64
+
+	c_a := table[0][0]
+	c_b := table[0][1]
+	c_c := table[1][0]
+	c_d := table[1][1]
+	N := c_a + c_b + c_c + c_d
+
+	if table[0][0] > 5 && table[0][1] > 5 && table[1][0] > 5 && table[1][1] > 5 && table[0][0]+table[0][1]+table[1][0]+table[1][1] >= 40 {
+		ChiSquare = stat.ChiSquare(table[0][:], table[1][:])
+	} else if table[0][0] >= 1 && table[0][1] >= 1 && table[1][0] >= 1 && table[1][1] >= 1 && table[0][0]+table[0][1]+table[1][0]+table[1][1] >= 40 {
+		ChiSquare = (math.Abs(c_a*c_d-c_b*c_c) - N*N/4) * (math.Abs(c_a*c_d-c_b*c_c) - N*N/4) / (c_a + c_b) / (c_c + c_d) / (c_a + c_c) / (c_b + c_d)
+	} else {
+		ChiSquare = combination(c_a+c_b, c_a) * combination(c_c+c_d, c_c) / combination(N, c_a+c_c)
+	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "数据已处理完毕", "chiSquare": ChiSquare})
+}
+
+func combination(n, k float64) float64 {
+	if k == 0 || k == n {
+		return 1
+	}
+	// 计算阶乘函数 math.factorial() 已废弃，使用 math.Gamma() 来近似计算阶乘
+	// 组合数公式：C(n, k) = n! / (k! * (n-k)!)
+	// 其中 Gamma 函数的近似值为 (n+1)!
+	return math.Gamma(float64(n+1)) / (math.Gamma(float64(k+1)) * math.Gamma(float64(n-k+1)))
 }
